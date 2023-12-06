dd43b9c47cbfe207f3749a0a4975f6973478c873
SJE <wodms1102@gmail.com>
2023-12-06 11:00:45 +0900

    commit

--- src/main/java/com/spacehub/www/dao/SmemberDAO.java
+++ src/main/java/com/spacehub/www/dao/SmemberDAO.java
@@ -47,42 +47,6 @@ public class SmemberDAO {
 			}
 			return vo;
 		}
-	
-	public SmemberVO getOne(String email) {
-		sb.setLength(0);
-<<<<<<< HEAD
-		sb.append("select memno,email,password,name,post,addr,account_num,regdate,credits,status from xe.smember where email=?");
-		SmemberVO vo = null;
-=======
-		sb.append("Select memno, email, name, post, addr, account_num, regdate, credits, status ");
-		sb.append("From xe.smember ");
-		sb.append("Where memno=?");
->>>>>>> refs/heads/master
-		
-		try {
-			pstmt = conn.prepareStatement(sb.toString());
-			pstmt.setString(1, email);
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				int memno = rs.getInt("memno");
-				String password = rs.getString("password");
-				String name = rs.getString("name");
-				String post = rs.getString("post");
-				String addr = rs.getString("addr");
-				String accountNum = rs.getString("account_num");
-				String regdate = rs.getString("regdate");
-				int credits = rs.getInt("credits");
-				int status = rs.getInt("status");
-				
-				vo = new SmemberVO(memno, email, password, name, post, addr, accountNum, regdate, credits, status);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return vo;
-	}
 		
 		public SmemberVO getOne(String email) {
 			sb.setLength(0);
@@ -184,77 +148,6 @@ public class SmemberDAO {
 				e.printStackTrace();
 			}
 		}
-	
-	public SmemberVO getOne(String email, String password) {
-		sb.setLength(0);
-		sb.append("select memno,email,password,name,post,addr,account_num,regdate,credits,status from xe.smember where email=? and password=?");
-		SmemberVO vo = null;
-		
-		try {
-			pstmt = conn.prepareStatement(sb.toString());
-			pstmt.setString(1, email);
-			pstmt.setString(2, password);
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				int memno = rs.getInt("memno");
-				String name = rs.getString("name");
-				String post = rs.getString("post");
-				String addr = rs.getString("addr");
-				String accountNum = rs.getString("account_num");
-				String regdate = rs.getString("regdate");
-				int credits = rs.getInt("credits");
-				int status = rs.getInt("status");
-				
-				vo = new SmemberVO(memno, email, password, name, post, addr, accountNum, regdate, credits, status);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return vo;
-	}
-	
-	// 추가
-	public void addOne(SmemberVO vo) {
-		sb.setLength(0);
-		sb.append("insert into xe.smember(email, password, name, post, addr, account_num, regdate, credits, status) ");
-		sb.append("values(?, ?, ?, ?, ?, ?, now(), ?, '1')");
-		
-		try {
-			pstmt = conn.prepareStatement(sb.toString());
-			
-			pstmt.setString(1, vo.getEmail());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getName());
-			pstmt.setString(4, vo.getPost());
-			pstmt.setString(5, vo.getAddr());
-			pstmt.setString(6, vo.getAccountNum());
-			pstmt.setInt(7, vo.getCredits());
-			
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	// 삭제
-	public void deleteOne(int memno) {
-		sb.setLength(0);
-		sb.append("delete from smember ");
-		sb.append("where memno=?");
-		
-		try {
-			pstmt = conn.prepareStatement(sb.toString());
-			pstmt.setInt(1, memno);
-			
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 	
 	// 종료
 	public void close() {
