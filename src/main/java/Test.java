92eef143d63afc811dd6ea63b25499edf44c779d
haeseok <haeseok_@naver.com>
2023-12-07 16:22:12 +0900

    메시지 기본 특 작

--- src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-import com.spacehub.www.dao.SpaceDAO;
-
-public class Test {
-	public static void main(String[] args) {
-		SpaceDAO dao = new SpaceDAO();
-		int a = dao.getLastInsert();
-		System.out.println(a);
-	}
-}
