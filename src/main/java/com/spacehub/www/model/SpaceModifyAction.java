1c897ab07e875b54072b7b1091dbad78eb488a9d
ysm <ysm5545@naver.com>
2023-12-12 18:20:39 +0900

    commit sm 23/12/12 18:20

--- src/main/java/com/spacehub/www/model/SpaceModifyAction.java
+++ src/main/java/com/spacehub/www/model/SpaceModifyAction.java
@@ -7,11 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.spacehub.www.dao.DiscountDAO;
 import com.spacehub.www.dao.SpaceDAO;
+import com.spacehub.www.dao.SpaceDetailDAO;
+import com.spacehub.www.dao.SpaceFacDAO;
 import com.spacehub.www.dao.SpaceImageDAO;
+import com.spacehub.www.vo.DiscountVO;
 import com.spacehub.www.vo.HostSpaceImageVO;
 import com.spacehub.www.vo.SmemberVO;
-import com.spacehub.www.vo.SpaceImageVO;
+import com.spacehub.www.vo.SpaceDetailVO;
+import com.spacehub.www.vo.SpaceFacVO;
 import com.spacehub.www.vo.SpaceVO;
 
 public class SpaceModifyAction implements Action {
@@ -19,18 +24,42 @@ public class SpaceModifyAction implements Action {
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		String memno = req.getParameter("memno");
 		String spaceno = req.getParameter("spaceno");
-		HttpSession session =  req.getSession();
+		HttpSession session =  req.getSession(true);
 		SmemberVO memberData = (SmemberVO) session.getAttribute("member");
+		
+		if (memberData == null) {
+		    return "/spaceHub/sign?cmd=login";  // 로그인 페이지로 리다이렉트 또는 포워드
+		}
+		
+		session.setAttribute("member", memberData);
+		
 		if(memno!=null && spaceno!=null && memberData!=null) {
 			SpaceDAO spaceDao = new SpaceDAO();
+			SpaceDetailDAO spaceDetailDao = new SpaceDetailDAO();
 			SpaceImageDAO spaceImageDao = new SpaceImageDAO();
+			SpaceFacDAO spaceFacDao = new SpaceFacDAO();
+			DiscountDAO discountDao = new DiscountDAO();
+			
 			SpaceVO spaceVo = spaceDao.getOne(Integer.parseInt(spaceno));
+			SpaceDetailVO spaceDetailVo = spaceDetailDao.getOne(Integer.parseInt(spaceno));
+			SpaceFacVO spaceFacVo = spaceFacDao.getOne(Integer.parseInt(spaceno));
 			ArrayList<HostSpaceImageVO> list = spaceImageDao.getSpaceImages(Integer.parseInt(spaceno),Integer.parseInt(memno));
+			ArrayList<DiscountVO> dcList = discountDao.getOne(Integer.parseInt(spaceno));
+			
 			req.setAttribute("spaceVo", spaceVo);
+			req.setAttribute("spaceDetailVo", spaceDetailVo);
+			req.setAttribute("spaceFacVo", spaceFacVo);
 			req.setAttribute("list", list);
-			session.setAttribute("member", memberData);
+			req.setAttribute("dcList", dcList);
+			for(int i=0; i<dcList.size(); i++) {
+				System.out.println(dcList.get(i));
+			}
+			
 			spaceDao.close();
+			spaceDetailDao.close();
 			spaceImageDao.close();
+			spaceFacDao.close();
+			discountDao.close();
 		}
 		return "/space/spaceModifyForm.jsp";
 	}
