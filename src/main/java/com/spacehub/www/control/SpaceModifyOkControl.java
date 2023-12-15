5c177471e3528c748776641b2cfcfd42cf3e7cab
ysm <ysm5545@naver.com>
2023-12-15 09:36:06 +0900

    Merge branch 'sm'

--- src/main/java/com/spacehub/www/control/SpaceModifyOkControl.java
+++ src/main/java/com/spacehub/www/control/SpaceModifyOkControl.java
@@ -34,7 +34,6 @@ public class SpaceModifyOkControl extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session =  req.getSession(true);
 		SmemberVO memberData = (SmemberVO) session.getAttribute("member");
-		session.setAttribute("member", memberData);
 		
 		SpaceDAO spaceDao = new SpaceDAO();
 		SpaceDetailDAO spcaeDetailDao = new SpaceDetailDAO();
@@ -62,28 +61,13 @@ public class SpaceModifyOkControl extends HttpServlet {
 			//지역 앞부분만 잘라오기
 			String loc = mr.getParameter("address");	
 			String[] locSplit = loc.split(" ");
-			//String loc1 = "경기 성남시 도곡동";
-			//String[] locSplit = loc1.split(" ");
 			String subject = mr.getParameter("subject");		
 			String post = mr.getParameter("post");		
 			String addr = mr.getParameter("address");		
 			String price = mr.getParameter("price");	
 			String vstatus	= mr.getParameter("vstatus");
-			String memno = mr.getParameter("memno");
-			//String regdate = req.getParameter("regdate");	
-			System.out.println("spaceno:"+spaceno);
-			System.out.println("spacetype:"+type);
-			System.out.println("spaceloc:"+locSplit[0]);
-			System.out.println("spacesubject:"+subject);
-			System.out.println("spacepost:"+post);
-			System.out.println("spacetype:"+type);
-			System.out.println("spaceaddr:"+addr);
-			System.out.println("spaceprice:"+price);
-			System.out.println("vstatus:"+vstatus);
-			System.out.println("memno:"+memno);
 			try {
 				String ip = Inet4Address.getLocalHost().getHostAddress();
-				System.out.println("spaceip:"+ip);
 			} catch (UnknownHostException e) {
 				System.out.println("space정보 가져오기 실패");
 				e.printStackTrace();
@@ -101,15 +85,6 @@ public class SpaceModifyOkControl extends HttpServlet {
 			String outDate = inOutDate.substring(idx+1);
 			String x = mr.getParameter("x");
 			String y = mr.getParameter("y");
-			System.out.println("spacedetailType:"+detailType);
-			System.out.println("detail:"+detail);
-			System.out.println("maxGuest:"+maxGuest);
-			System.out.println("bed:"+bed);
-			System.out.println("bathroom:"+bathroom);
-			System.out.println("inDate:"+inDate);
-			System.out.println("outDate:"+outDate);
-			System.out.println("x:"+x);
-			System.out.println("y:"+y);
 			
 			//공간 편의시설(space_fac)
 			String wifi = mr.getParameter("wifi");
@@ -126,26 +101,10 @@ public class SpaceModifyOkControl extends HttpServlet {
 			String firealarm = mr.getParameter("firealarm");
 			String aidkit = mr.getParameter("aidkit");
 			String firearm = mr.getParameter("firearm");
-			System.out.println("wifi:"+wifi);
-			System.out.println("tv:"+tv);
-			System.out.println("kitchen:"+kitchen);
-			System.out.println("wm:"+wm);
-			System.out.println("aircon:"+aircon);
-			System.out.println("canpark:"+canpark);
-			System.out.println("canfreepark:"+canfreepark);
-			System.out.println("swim:"+swim);
-			System.out.println("bbq:"+bbq);
-			System.out.println("pooltable:"+pooltable);
-			System.out.println("fireplace:"+fireplace);
-			System.out.println("firealarm:"+firealarm);
-			System.out.println("aidkit:"+aidkit);
-			System.out.println("firearm:"+firearm);
 			
 			//할인등록
 			String[] dcRatio = mr.getParameterValues("dcRatio");
 			String[] disName = mr.getParameterValues("disName");
-			System.out.println("dcRatio:"+dcRatio);
-			System.out.println("disName:"+disName);
 ///////////////////////vo.set, modifyOne/////////////////////////
 
 			//공간 정보(space) 수정
@@ -164,7 +123,7 @@ public class SpaceModifyOkControl extends HttpServlet {
 			}
 			spaceVo.setVStatus(Integer.parseInt(vstatus));
 			//spaceVo.setStatus(Integer.parseInt(status));
-			spaceVo.setMemno(Integer.parseInt(memno));
+			spaceVo.setMemno(memberData.getMemno());
 			spaceDao.modifyOne(spaceVo);
 			
 			//공간 상세정보(space_detail)
@@ -182,10 +141,6 @@ public class SpaceModifyOkControl extends HttpServlet {
 					
 		
 			// 공간 이미지(space_image)
-			System.out.println(mr.getFileNames());
-			System.out.println(mr.getFilesystemName("img"));
-			System.out.println(mr.getOriginalFileName("img"));
-			
 			// 첫 번째 파일 처리
 		    Enumeration<String> fileInputNames = mr.getFileNames(); // 모든 파일 업로드 필드의 이름들을 가져옴
 
@@ -196,9 +151,6 @@ public class SpaceModifyOkControl extends HttpServlet {
 		        String inputName = fileInputNames.nextElement();
 		        String uploadedFilePath = mr.getOriginalFileName(inputName);
 		        String imgIdx = inputName.substring(3);//img(?) 번호 가져오기
-		        System.out.println("inputName:" + inputName );
-		        System.out.println("uploadedFilePath : " + uploadedFilePath);
-		        System.out.println("imgIdx: "+ imgIdx);
 		        
 		        // 파일 업로드가 실패한 경우, 파일 경로는 null일 수 있습니다.
 		        if (uploadedFilePath != null) {
@@ -209,7 +161,6 @@ public class SpaceModifyOkControl extends HttpServlet {
 		            spaceImageVo.setPath("/spaceHub/upload/" + uploadedFilePath ); // 파일 경로 설정
 
 		            // 해당 파일 정보를 DB에 삭제 후 추가
-		            //System.out.println("이미지 VO : " + spaceImageVo);
 		            spcaeImageDao.addOne(spaceImageVo);
 		        }
 		    } //while end
